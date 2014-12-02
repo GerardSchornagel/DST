5416b337713052ea9d93d181f211b4f1f13e8d3a
Gerard Evert Schornagel <gschornagel@hotmail.com>
2014-12-02 23:52:10 +0100

    Added homemade .ini file handler. And a bit of directory structrizing.

---
+++ Functions/iniFileHandler.vb
@@ -0,0 +1,67 @@
+﻿''' <summary>
+''' Load and Save of ini-files.
+''' </summary>
+Public Class iniFileHandler
+	Private stringAssembled(,) As String
+	Private stringMediator() As String
+	Private stringBody() As String
+	Private stringIO As String
+	Private integerMediator As Integer
+	Private integerDimension1 As Integer
+	Private integerDimension2 As Integer
+	
+	Public Function Load(Filepath As String) As String(,)
+		'Take stringIO from file.
+		Try
+		Using binaryReader As New System.IO.BinaryReader(System.IO.File.Open(Filepath, System.IO.FileMode.Open))
+			Do
+				stringIO += binaryReader.ReadChar
+			Loop
+		End Using
+		Catch ex As System.IO.EndOfStreamException
+		End Try
+		
+		stringMediator = System.Text.RegularExpressions.Regex.Split(stringIO, (Chr(13) & Chr(10)))
+		ReDim stringAssembled(stringMediator.GetUpperBound(0), 5)
+		integerMediator = 0
+		integerDimension1 = -1
+		integerDimension2 = 0
+		'*****ASSEMBLED FIRST BOUND TO BIG; MAYVBE COUNT "[" CHARS AND USE AS***
+		Do
+			'Check each entry for ""
+			If stringMediator(integerMediator) = "" Then
+				'Check each entry for "["
+			ElseIf stringMediator(integerMediator).StartsWith("[") Then
+				'TRUE=stringAssembled(#,) minus last "]"
+				integerDimension1 += 1
+				integerDimension2 = 0
+				StringAssembled(integerDimension1, integerDimension2) = (stringMediator(integerMediator).remove(stringMediator(integerMediator).length - 1, 1)).Remove(0, 1)
+				integerDimension2 = 1
+			Else
+				'FALSE=stringAssembled(,#)
+				stringAssembled(integerDimension1, integerDimension2) = stringMediator(integerMediator)
+				integerDimension2 += 1
+			End If
+			integerMediator += 1
+		Loop Until integerMediator > stringMediator.GetUpperBound(0)
+		'*****RETHINK STRATEGY: FIRST SPLIT IS BY RETURN AND LINE FEED****
+		'Split stringIO to stringMediator() by opening bracket.
+		'Split array1D(#) after closing bracket(1) = Split to arrayRow() by ";"
+		'Get Upperbound of array1D and arrayRow = ReDim stringAssembled(,)
+		'Split array1D(#) before closing bracket(0) = stringAssembled(#,) 'Maybe include "/r/n" [or just take next 4 Chr()]
+		'Build from arrayRow() to stringAssembled(,#)
+		'Rinse and repeat from step 3 and skip 4.
+		Return stringAssembled
+	End Function
+	
+	Public Sub Save(Filepath As String, SaveData(,) As String)
+		'Get Upperbound(0&1) = ReDim arrayRow() & array1D()
+		'Write open bracket to stringIO
+		'Write 1st dimension from stringAssembled(#,) to stringIO
+		'Write closing bracket to stringIO
+		'Build from stringAssembled(,#) to arrayRow(#)
+		'Write arrayRow() to stringIO glued by ";"
+		'Rinse and repeat from step 2.
+		'Start stringWriter
+	End Sub
+End Class
---
+++ Functions/iniHandler.vb
@@ -0,0 +1,117 @@
+﻿''' <summary>
+''' Will write/read 2D string-array's to/from read-able .ini files.
+''' </summary>
+Public Class iniHandler
+	Private stringIO As String
+	Dim stringMediator() As String
+	Dim stringOutput(,) As String
+	
+	Private integerColumn As Integer
+	Private integerRow As Integer
+	Private integerMediator As Integer
+	''' <summary>
+	''' Saves a 2d string-array to directory and file.
+	''' </summary>
+	''' <param name="Pathname">Complete Path and File.</param>
+	''' <param name="SavingFile">Input 2D string-array.</param>
+	Public Sub Save(Pathname As String, SavingFile(,) As String)
+		'Prepare variable's for new content.
+		ReDim stringMediator((((SavingFile.GetUpperBound(0) + 1) * (SavingFile.GetUpperBound(1) + 1)) - 1) * 2)
+		stringIO = ""
+		integerColumn = 0
+		integerRow = 0
+		integerMediator = 0
+		
+		Do 'Loops all the Columns of the table.
+			Do 'Loops all the Row Items.
+				If SavingFile(integerColumn, integerRow) = "" Then 'Just continue next row.
+				Else 'Save the row item to the stringMediator
+					stringMediator(integerMediator) = SavingFile(integerColumn, integerRow)
+					integerMediator += 1
+				End If
+					integerRow += 1
+			Loop Until integerRow > SavingFile.GetUpperBound(1)
+			'Prepare variable's for next Do for next Column.
+			integerRow = 0
+			integerColumn += 1
+			stringMediator(integerMediator) = ""
+			integerMediator += 1
+		Loop Until integerColumn > SavingFile.GetUpperBound(0)
+		
+		'Add the Column max. bound to the file.
+		If SavingFile.GetUpperBound(0) > 9 Then
+			stringIO &= SavingFile.GetUpperBound(0)
+		Else
+			stringIO &= "0" & SavingFile.GetUpperBound(0)
+		End If
+		'Add the Row max. bound to the file.
+		If SavingFile.GetUpperBound(1) > 9 Then
+			stringIO &= SavingFile.GetUpperBound(1)
+		Else
+			stringIO &= "0" & SavingFile.GetUpperBound(1)
+		End If
+		'Loop through all stringMediator items and write as 1 string.
+		integerMediator = 0
+		Do
+			stringIO &= stringMediator(integerMediator) & Chr(13) & Chr(10)
+			integerMediator += 1
+		Loop Until integerMediator > stringMediator.GetUpperBound(0)
+		stringIO.Remove(stringIO.Length - 2, 2) 'Remove last two Chr's
+		'Use StringBuilder for writing
+		Dim stringbuilderIO As New System.Text.StringBuilder()
+		Using streamreaderIO As New System.IO.StringReader(stringIO)
+			stringbuilderIO.Append(streamreaderIO.ReadToEnd())
+		End Using
+        Using streamWriter As New System.IO.StreamWriter(Pathname, False, System.Text.Encoding.Default)
+            streamWriter.Write(stringbuilderIO.ToString())
+        End Using
+	End Sub
+	
+	''' <summary>
+	''' Load an 2d string-array from a .ini file.
+	''' </summary>
+	''' <param name="Pathname">Complete Path and File.</param>
+	''' <returns>Returns a 2D string-array.</returns>
+	Public Function Load(Pathname As String) As String(,)
+		stringIO = ""
+		Using streamReader As New System.IO.StreamReader(Pathname)
+			stringIO = streamReader.ReadToEnd()
+		End Using
+		
+		ReDim stringOutput(CType(stringIO.Substring(0, 2), Integer), CType(stringIO.Substring(2, 2), Integer))
+		stringIO = stringIO.Remove(0, 4)
+		
+		stringMediator = stringIO.Split(Chr(10))
+		integerMediator = 0
+		For Each line In stringMediator
+			If stringMediator(integerMediator) = "" Then
+				integerMediator += 1
+				Continue For
+			End If
+			stringMediator(integerMediator) = line.Remove(line.Length - 1, 1)
+			integerMediator += 1
+		Next
+		integerColumn = -1
+		integerRow = 0
+		integerMediator = 0
+		
+		Do
+			If stringMediator(integerMediator) = "" Then
+				integerMediator += 1
+				Continue Do
+			ElseIf stringMediator(integerMediator).Substring(0, 1) = "[" Then
+				integerColumn += 1
+				integerRow = 0
+				stringOutput(integerColumn, integerRow) = stringMediator(integerMediator)
+				integerRow += 1
+				integerMediator += 1
+			Else
+				stringOutput(integerColumn, integerRow) = stringMediator(integerMediator)
+				integerMediator += 1
+				integerRow += 1
+			End If
+			
+		Loop Until integerMediator > stringMediator.GetUpperBound(0)
+		Return stringOutput
+	End Function
+End Class
