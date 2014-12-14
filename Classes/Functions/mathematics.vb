3338c123b1c45f1d46ec53fabe7edef9a9e883fa
Gerard Evert Schornagel <gschornagel@hotmail.com>
2014-12-14 02:33:31 +0100

    Moved constructed code from Form to usable Module.

---
+++ Classes/Functions/mathematics.vb
@@ -0,0 +1,37 @@
+﻿''' <summary>
+''' Contains various math-like functions.
+''' </summary>
+Public Module mathematics
+	Public Function CustomerBuying As String
+			'Make new Customer and get his/her data for log
+			Dim integerStoreLevel, integerStoreShelf, integerStoreBin, integerBinQuantity, integerBinPrice As Integer
+			Dim stringBinName, stringReturn As String
+			Dim randomGenerator As New Random()
+			Dim CustomerNew As New customer
+			CustomerNew.newCustomer()
+			
+			integerStoreLevel = randomGenerator.Next(store.arrayLevel.GetUpperBound(0))
+			integerStoreShelf = randomGenerator.Next(store.arrayLevel(integerStoreLevel).arrayShelf.GetUpperBound(0))
+			integerStoreBin = randomGenerator.Next(store.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin.GetUpperBound(0))
+			stringBinName = cache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin(integerStoreBin).ArticleName
+			integerBinQuantity = cache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin(integerStoreBin).BinQuantity
+			integerBinPrice = cache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin(integerStoreBin).ArticleLastSell
+			
+			If integerBinQuantity > 0 Then 'Check for available quantity.
+				If integerBinPrice <= CustomerNew.Money Then 'BUY:Check for customer-money and item sell-price
+					stringReturn = stringBinName & " sold to " & CustomerNew.Name & " ($ " & CustomerNew.Money & ") from slot " & integerStoreBin & Chr(10)
+					cache.playerCharacter.Balance += integerBinPrice
+					cache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin(integerStoreBin).BinQuantity -= 1
+					cache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin(integerStoreBin).BinSave(cache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).ShelfPath)
+					cache.playerCharacter.TotalItemsSold += 1
+'					
+				ElseIf integerBinPrice >= CustomerNew.Money Then 'NOCASH:Check for customer-money and item sell-price
+					stringReturn = stringBinName & " too Expensive for " & CustomerNew.Name & " ($ " & CustomerNew.Money & ") from slot " & integerStoreBin & Chr(10)
+				End If
+				
+			ElseIf integerBinQuantity = 0 Then 'NOSTASH:Check for customer-money and item sell-price
+				stringReturn = stringBinName & " stock Depleted on slot " & integerStoreBin & Chr(10)
+			End If
+			Return stringReturn
+	End Function
+End Module
--- Forms/FormGame.vb
+++ Forms/FormGame.vb
@@ -3,14 +3,6 @@
 	Private timerHour As New System.Windows.Forms.Timer
 	Private timerCustomer As New System.Windows.Forms.Timer
 	Private integerCustomerNumber As Integer
-	Private integerStoreLevels As Integer
-	Private integerStoreShelfs As Integer
-	Private integerStoreBins As Integer
-	Private stringBinName As String
-	Private integerBinQuantity As Integer
-	Private integerBinPrice As Integer
-	
-	Private CustomerNew As New customer()
 	
 	Public Sub New()
 		' The Me.InitializeComponent call is required for Windows Forms designer support.
@@ -57,32 +49,7 @@
 	
 	Private Sub TimerCustomer_Tick(ByVal sender As Object, ByVal e As System.EventArgs)
 		If IntegerCustomerNumber > 0 Then 'More then Zero
-			'Make new Customer and get his/her data for log
-			CustomerNew.newCustomer
-			
-			integerStoreLevels = randomGenerator.Next(store.arrayLevel.GetUpperBound(0))
-			integerStoreShelfs = randomGenerator.Next(store.arrayLevel(integerStoreLevels).arrayShelf.GetUpperBound(0))
-			integerStoreBins = randomGenerator.Next(store.arrayLevel(integerStoreLevels).arrayShelf(integerStoreShelfs).arrayBin.GetUpperBound(0))
-			stringBinName = cache.playerStore.arrayLevel(integerStoreLevels).arrayShelf(integerStoreShelfs).arrayBin(integerStoreBins).ArticleName
-			integerBinQuantity = cache.playerStore.arrayLevel(integerStoreLevels).arrayShelf(integerStoreShelfs).arrayBin(integerStoreBins).BinQuantity
-			integerBinPrice = cache.playerStore.arrayLevel(integerStoreLevels).arrayShelf(integerStoreShelfs).arrayBin(integerStoreBins).ArticleLastSell
-						
-			If integerBinQuantity > 0 Then 'Check for available quantity.
-				If integerBinPrice <= CustomerNew.Money Then 'BUY:Check for customer-money and item sell-price
-					textboxActionLog.AppendText("[" & textboxActionHour.Text & "] " & stringBinName & " sold to " & CustomerNew.Name & " ($ " & CustomerNew.Money & ") from slot " & integerStoreBins & Chr(10))
-					cache.playerCharacter.Balance += integerBinPrice
-					cache.playerStore.arrayLevel(integerStoreLevels).arrayShelf(integerStoreShelfs).arrayBin(integerStoreBins).BinQuantity -= 1
-					cache.playerStore.arrayLevel(integerStoreLevels).arrayShelf(integerStoreShelfs).arrayBin(integerStoreBins).BinSave(cache.playerStore.arrayLevel(integerStoreLevels).arrayShelf(integerStoreShelfs).ShelfPath)
-					cache.playerCharacter.TotalItemsSold += 1
-'					
-				ElseIf integerBinPrice >= CustomerNew.Money Then 'NOCASH:Check for customer-money and item sell-price
-					textboxActionLog.AppendText("[" & textboxActionHour.Text & "] " & stringBinName & " too Expensive for " & CustomerNew.Name & " ($ " & CustomerNew.Money & ") from slot " & integerStoreBins & Chr(10))
-				End If
-				
-			ElseIf integerBinQuantity = 0 Then 'NOSTASH:Check for customer-money and item sell-price
-				textboxActionLog.AppendText("[" & textboxActionHour.Text & "] " & stringBinName & " stock Depleted on slot " & integerStoreBins & Chr(10))
-			End If
-			
+			textboxActionLog.AppendText("[" & textboxActionHour.Text & "] " & mathematics.CustomerBuying)
 			IntegerCustomerNumber -= 1
 		Else 'Less then zero customers
 			timerCustomer.Stop()
