ef6eb46b28e0170a70a335443941396a0545122f
Gerard Evert Schornagel <gschornagel@hotmail.com>
2014-12-30 17:39:03 +0100

    Added simple Customer-desire calculation in mathematics.vb.

--- Classes/Functions/mathematics.vb
+++ Classes/Functions/mathematics.vb
@@ -9,32 +9,44 @@ Public Module mathematics
 			Dim randomGenerator As New Random()
 			Dim CustomerNew As New customer
 			CustomerNew.newCustomer()
-			
+			'Setting Random's for slot choosing.
 			stringReturn = "0"
 			integerStoreLevel = randomGenerator.Next(store.arrayLevel.GetUpperBound(0))
 			integerStoreShelf = randomGenerator.Next(store.arrayLevel(integerStoreLevel).arrayShelf.GetUpperBound(0))
-			Dim temp As Integer = (store.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin.GetUpperBound(0))			
-			integerStoreBin = randomGenerator.Next(temp + 1)
+			integerStoreBin = randomGenerator.Next((store.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin.GetUpperBound(0)) + 1)
+			'Getting Bin data to local variable's.
 			stringBinName = gamecache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin(integerStoreBin).ArticleName
 			integerBinQuantity = gamecache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin(integerStoreBin).BinQuantity
 			integerBinPrice = gamecache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin(integerStoreBin).ArticleLastSell
-			
-			If integerBinQuantity > 0 Then 'Check for available quantity.
-				If integerBinPrice <= CustomerNew.Money Then 'BUY:Check for customer-money and item sell-price
-					stringReturn = stringBinName & " sold to " & CustomerNew.Name & " ($ " & CustomerNew.Money & ") from slot " & integerStoreBin & Chr(10)
-					gamecache.playerCharacter.Balance += integerBinPrice
-					gamecache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin(integerStoreBin).BinQuantity -= 1
-					gamecache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin(integerStoreBin).BinSave(gamecache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).ShelfPath)
-					gamecache.playerCharacter.TotalItemsSold += 1
-					gamecache.playerCharacter.TotalEarnings += integerBinPrice
-'					
-				ElseIf integerBinPrice >= CustomerNew.Money Then 'NOCASH:Check for customer-money and item sell-price
+			'First check for quantity in Bin
+			If integerBinQuantity > 0 Then 'STASH
+				'Check for enough CustomerMoney
+				If integerBinPrice <= CustomerNew.Money Then 'BUY
+					If ((CustomerNew.Money / 100) * CustomerNew.Desire) >= integerBinPrice Then 'INTRESTED
+						'set Return for formStatus.textboxLog
+						stringReturn = stringBinName & " sold to " & CustomerNew.Name & " ($ " & CustomerNew.Money & ") from slot " & integerStoreBin & Chr(10)
+						'Add Article Sell price to Balance
+						gamecache.playerCharacter.Balance += integerBinPrice
+						'Remove 1 item from selected Bin and Saves.
+						gamecache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin(integerStoreBin).BinQuantity -= 1
+						gamecache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).arrayBin(integerStoreBin).BinSave(gamecache.playerStore.arrayLevel(integerStoreLevel).arrayShelf(integerStoreShelf).ShelfPath)
+						'Add data to Statistics.
+						gamecache.playerCharacter.TotalItemsSold += 1
+						gamecache.playerCharacter.TotalEarnings += integerBinPrice
+					Else 'NOTINTRESTRED
+						stringReturn = stringBinName & " didn't sell to " & CustomerNew.Name & ", because of no desire from slot " & integerStoreBin & Chr(10)
+					End If
+					
+				ElseIf integerBinPrice >= CustomerNew.Money Then 'TOOEXPENSIVE
+					'Set Return for formStatus.textboxLog
 					stringReturn = stringBinName & " too Expensive for " & CustomerNew.Name & " ($ " & CustomerNew.Money & ") from slot " & integerStoreBin & Chr(10)
 				End If
 				
-			ElseIf integerBinQuantity = 0 Then 'NOSTASH:Check for customer-money and item sell-price
+			ElseIf integerBinQuantity = 0 Then 'NOSTASH
+				'Set Return for formStatus.textboxLog
 				stringReturn = stringBinName & " stock Depleted on slot " & integerStoreBin & Chr(10)
 			End If
+			'Return end result.
 			Return stringReturn
 	End Function
 End Module
