77ff6d28c3657bfaafdc59dcb4ec1291c83bd345
Gerard Evert Schornagel <gschornagel@hotmail.com>
2016-02-22 06:43:24 +0100

    Fixed: \Classes\Functions\binaryFileHandler.cs
    Removed PeekChar and used file-length for better performance.

--- Game/Classes/Functions/binaryFileHandler.cs
+++ Game/Classes/Functions/binaryFileHandler.cs
@@ -6,14 +6,15 @@ using System;
 /// </summary>
 public class binaryFileHandler
 {
+    private string stringStream;
     private string stringLine;
     private string[] stringRow;
     private string[,] stringTable;
 
-    private string stringStream;
     private int integerRawCounter;
     private int integerRowCounter;
     private int integerColumnCounter;
+
     /// <summary>
     /// Initialize class.
     /// </summary>
@@ -21,6 +22,7 @@ public class binaryFileHandler
     {
 
     }
+
     /// <summary>
     /// Save input to file on given location.
     /// </summary>
@@ -30,7 +32,7 @@ public class binaryFileHandler
     /// <param name="row">Simple array's.</param>
     /// <param name="table">2D array's.</param>
     /// <param name="bound">Bound index for 2D array's.</param>
-    public void Save(string path, string file, string line = null, string[] row = null, string[,] table = null, int bound = -1)
+    public void Save(string path, string file, string line = null, string[] row = null, string[,] table = null, int bound = 0)
     {
         //Make directory
         System.IO.Directory.CreateDirectory(path);
@@ -83,6 +85,7 @@ public class binaryFileHandler
         integerColumnCounter = 0;
         int32Stream = null;
     }
+
     /// <summary>
     /// Load given file to String.
     /// </summary>
@@ -91,25 +94,27 @@ public class binaryFileHandler
     /// <returns>One-line String.</returns>
     public string LoadLine(string path, string file)
     {
-        string functionReturnValue = null;
+        stringStream = "";
         //Check for existing file.
-        if (System.IO.File.Exists(path + file) == false) {
-            Interaction.MsgBox(path + file + " doesn't exist..." + Char(10) + "Aborting current load, going to next command.");
+        if (System.IO.File.Exists(path + file)) {
+        } else {
+            Interaction.MsgBox(path + file + " doesn't exist..." + (char)10 + "Aborting current load, going to next command.");
             return null;
-            return functionReturnValue;
         }
         //Get all info from File to String.
         using (System.IO.BinaryReader binReader = new System.IO.BinaryReader(System.IO.File.Open(path + file, System.IO.FileMode.Open))) {
-            do {
-                stringStream += (Char(binReader.ReadInt32));
-            } while (!(binReader.PeekChar == null));
+            
+            int pos = 0;
+            int length = (int)binReader.BaseStream.Length;
+            while (pos < length) {
+                stringStream += char.ConvertFromUtf32(binReader.ReadInt32());
+                // Advance our position variable.
+                pos += sizeof(int);
+            }
         }
-
         return stringStream;
-        //Resetting variable
-        stringStream = "";
-        return functionReturnValue;
     }
+    
     /// <summary>
     /// Load given file to String().
     /// </summary>
@@ -118,30 +123,32 @@ public class binaryFileHandler
     /// <returns>1D String array.</returns>
     public string[] LoadRow(string path, string file)
     {
-        string[] functionReturnValue = null;
+        //Resetting variable
         stringStream = "";
         stringRow = null;
+
         //Check for existing file.
-        if (System.IO.File.Exists(path + file) == false) {
-            Interaction.MsgBox(path + file + " doesn't exist..." + Char(10) + "Aborting current load, going to next command.");
+        if (System.IO.File.Exists(path + file)) {
+        } else {
+            Interaction.MsgBox(path + file + " doesn't exist..." + (char)10 + "Aborting current load, going to next command.");
             return null;
-            return functionReturnValue;
         }
+
         //Get all info from File to String.
         using (System.IO.BinaryReader binReader = new System.IO.BinaryReader(System.IO.File.Open(path + file, System.IO.FileMode.Open))) {
-            do {
-                stringStream += (Char(binReader.ReadInt32));
-            } while (!(binReader.PeekChar == null));
+            int pos = 0;
+            int length = (int)binReader.BaseStream.Length;
+            while (pos < length) {
+                stringStream += char.ConvertFromUtf32(binReader.ReadInt32());
+                // Advance our position variable.
+                pos += sizeof(int);
+            }
         }
         //Split raw data in String-Array
         stringRow = stringStream.Split(new string[] { "<>" }, StringSplitOptions.None);
         return stringRow;
-
-        //Resetting variable
-        stringStream = "";
-        stringRow = null;
-        return functionReturnValue;
     }
+
     /// <summary>
     /// Load given file to String(,)
     /// </summary>
@@ -151,19 +158,32 @@ public class binaryFileHandler
     /// <returns>2D String array.</returns>
     public string[,] LoadTable(string path, string file, int bound)
     {
-        string[,] functionReturnValue = null;
+        //Resetting variable
+        stringStream = "";
+        stringRow = null;
+        stringTable = null;
+        integerRowCounter = 0;
+        integerColumnCounter = 0;
+        integerRawCounter = 0;
+
         //Check for existing file.
-        if (System.IO.File.Exists(path + file) == false) {
-            Interaction.MsgBox(path + file + " doesn't exist..." + Char(10) + "Aborting current load, going to next command.");
+        if (System.IO.File.Exists(path + file)) {
+        } else {
+            Interaction.MsgBox(path + file + " doesn't exist..." + (char)10 + "Aborting current load, going to next command.");
             return null;
-            return functionReturnValue;
         }
+
         //Get all info from File to String.
         using (System.IO.BinaryReader binReader = new System.IO.BinaryReader(System.IO.File.Open(path + file, System.IO.FileMode.Open))) {
-            do {
-                stringStream += (Char(binReader.ReadInt32));
-            } while (!(binReader.PeekChar == null));
+            int pos = 0;
+            int length = (int)binReader.BaseStream.Length;
+            while (pos < length) {
+                stringStream += char.ConvertFromUtf32(binReader.ReadInt32());
+                // Advance our position variable.
+                pos += sizeof(int);
+            }
         }
+
         //Split raw data in String-Array
         stringRow = stringStream.Split(new string[] { "<>" }, StringSplitOptions.None);
         stringTable = new string[Convert.ToInt32(((stringRow.GetUpperBound(0) + 1) / (bound + 1)) - 1) + 1, bound + 1];
@@ -176,15 +196,6 @@ public class binaryFileHandler
             integerColumnCounter += 1;
             integerRowCounter = 0;
         } while (!(integerColumnCounter > stringTable.GetUpperBound(0)));
-
         return stringTable;
-        //Resetting variable
-        stringStream = "";
-        stringRow = null;
-        stringTable = null;
-        integerRowCounter = 0;
-        integerColumnCounter = 0;
-        integerRawCounter = 0;
-        return functionReturnValue;
     }
 }
